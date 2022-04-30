31be4b1a9d5dfd421d47999f3362d5444914d701
Alen Duda <alenduda@gmail.com>
2022-04-30 19:06:42 +0200

    enable cors

--- pages/api/legacy/history.ts
+++ pages/api/legacy/history.ts
@@ -1,7 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import initMiddleware from '../../../src/middleware/init-middleware';
+import Cors from 'cors';
 import { findLatestShows } from '../../../src/utils/legacy/crawler';
 
+const cors = initMiddleware(
+    Cors({
+        methods: ['GET']
+    })
+);
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    await cors(req, res);
     try {
         const shows = await findLatestShows('povijest-cetvrtkom');
         return res.status(200).json(shows);
--- pages/api/legacy/latest-news.ts
+++ pages/api/legacy/latest-news.ts
@@ -1,7 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import initMiddleware from '../../../src/middleware/init-middleware';
+import Cors from 'cors';
 import { getUrls } from '../../../src/utils/legacy/crawler';
 
+const cors = initMiddleware(
+    Cors({
+        methods: ['GET']
+    })
+);
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    await cors(req, res);
     try {
         const newsUrls = await getUrls();
         return res.status(200).json(newsUrls);
